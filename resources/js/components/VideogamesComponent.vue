6411bf668031cefd92ee365934298edd9b20df5f
DragoAl <88578768+DragoAl@users.noreply.github.com>
2022-02-14 18:23:24 +0100

    Add vue components videogames

---
+++ resources/js/components/VideogamesComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="container">
+        <h1>Videogames List</h1>
+         <div class="videogame-cont" v-for="videogame in videogames" :key="videogame.id">
+             <h3>{{videogame.title}}</h3>
+             <span>{{videogame.subtitle}}</span>
+             <h5>Company: {{videogame.editor}}</h5>
+             <span v-if="(videogame.ratings)">Ratings: {{videogame.ratings}}</span>
+         </div>
+        
+
+    </div>
+</template>
+<script>
+export default {
+    
+    data(){
+        return {
+            videogames: [] 
+        }
+    },
+    mounted() {
+        axios.get('/api/videogames/list')
+            .then(res => this.videogames = res.data )
+            .catch(err => console.error(err));
+
+        
+    }
+
+}
+</script>
+<style lang="scss" scoped="scss">
+#container {
+    width: 60%;
+    margin: 0 auto;
+}
+.videogame-cont {
+    margin: 15px auto;
+    border: 2px solid black;
+    text-align: center;
+}
+
+</style>
